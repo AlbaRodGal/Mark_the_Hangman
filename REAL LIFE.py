a1a8075a989f4ad996b0c0c6633eaf9ac4a1e29d
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2019-09-25 17:11:58 +0100

    Function to hide selected_word

--- REAL LIFE.py
+++ REAL LIFE.py
@@ -4,30 +4,30 @@ import random
 
 # Welcome and instructions:
 
-welcome = "Hello from the other side!"
-instructions = """Can you guess the word?"""
+def print_instructions(welcome, instructions):
+    print(welcome.center(150))
+    print(instructions.center(150))
+    print()
+    
+# Choose random word of list_of_words
 
-print(welcome.center(150))
-print(instructions.center(150))
-print()
+def generate_random_word(list_of_words):
+    random_word = random.choice(list_of_words)
+    selected_word = list(random_word.rstrip("\n"))
+    return selected_word
 
 # Import file
 
 with open("/Users/albarodriguez/Projects/Mark_the_Hangman/ListOfWords.txt") as f:
     list_of_words = f.readlines()
 
-# Choose random word of list_of_words
-def generate_random_word(list_of_words):
-    random_word = random.choice(list_of_words)
-    selected_word = list(random_word.rstrip("\n"))
-    print(selected_word)
-    return selected_word
+# Function to hide selected_word
 
-generate_random_word(list_of_words)
+def hide_word(selected_word):
+    number_of_letters = len(selected_word)
+    hidden_word = ["_"] * len(selected_word)
+    return hidden_word
 
-# Hide random_word
-
-number_of_letters = len(selected_word)
 print(f"Hint: {number_of_letters} letters")
 
 lives = 5
@@ -35,8 +35,6 @@ lives = 5
 print(f"Lives: {lives}")
 
 print()
-
-hidden_word = ["_"] * len(selected_word)
 
 # Function to look for user's input in selected word
 def input():
@@ -85,3 +83,24 @@ def check_user_attempt():
     for index, letters in enumerate(attempt):
         if user_attempt is selected_word:
             user_attempt[index] == selected_word[index]
+
+
+########################## M a i n ##################################
+
+# Import file
+
+with open("/Users/albarodriguez/Projects/Mark_the_Hangman/ListOfWords.txt") as f:
+    list_of_words = f.readlines()
+
+#### Definition of variables ####
+
+welcome = "Hello from the other side!"
+instructions = """Can you guess the word?"""
+random_word = random.choice(list_of_words)
+selected_word = list(random_word.rstrip("\n"))
+
+#### Working Programme ####
+
+print_instructions(welcome,instructions)
+generate_random_word(list_of_words)
+hide_word(selected_word)
