8db4ae60499d8e29a9367e1596a1c7d153b5d112
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2019-09-25 06:50:27 +0100

    Function to print instructions and function to generate word 2

--- REAL LIFE.py
+++ REAL LIFE.py
@@ -1,4 +1,4 @@
-"Second attempt of builing a word guessing game."
+"Second attempt of building a word guessing game."
 
 import random
 
@@ -13,16 +13,17 @@ print()
 
 # Import file
 
-with open("C:/Users/Alba/PycharmProjects/MarkTheHangman/ListOfWords.txt") as f:
+with open("/Users/albarodriguez/Projects/Mark_the_Hangman/ListOfWords.txt") as f:
     list_of_words = f.readlines()
 
 # Choose random word of list_of_words
+def generate_random_word(list_of_words):
+    random_word = random.choice(list_of_words)
+    selected_word = list(random_word.rstrip("\n"))
+    print(selected_word)
+    return selected_word
 
-random_word = random.choice(list_of_words)
-selected_word = list(random_word.rstrip("\n"))
-print(selected_word)
-
-# print(selected_word)
+generate_random_word(list_of_words)
 
 # Hide random_word
 
@@ -36,49 +37,51 @@ print(f"Lives: {lives}")
 print()
 
 hidden_word = ["_"] * len(selected_word)
-# print(number_of_letters * "_ ")
-# print()
 
-# User to type letter or word
+# Function to look for user's input in selected word
+def input():
+    for index in range(0,10):
+        print_hidden_word()
+        input_letter = input("Please type a letter or word:  ")
 
-# input_letter = input("Please type a letter or word:  ")
-
-# if input_letter.isalpha():
-   # print("True")
-# else:
-    #print("Sorry, I didn't understand that")
-
-# Funtion to print an updated version hidden_word
-def print_hidden_word():
-    print(" ".join([letter for letter in hidden_word])) # .join converts hidden_word in string
-
-# Look for user's input in selected word
-
-for index in range(0,10):
-    print_hidden_word()
-    input_letter = input("Please type a letter or word:  ")
-    # Validation of user's input
+# Function to validate and check user's input
+def check_input():    
     if input_letter.isalpha():
         print()
     else:
         print("Sorry, I didn't understand that")
-
     if input_letter in selected_word:
         print("Well Done!")
         for index, letter in enumerate(selected_word):
             if letter is input_letter:
                 hidden_word[index] = input_letter
-                input_word = input("Can you guess the word?(Yes/No): ")
-                if input_word == "yes":
-                    attempt = input("Type the word: ")
-                else:
-                    print()
+            
     else:
         print("No, sorry")
         lives -= 1
         print(f"Lives: {lives}")
-        if lives == 0:
-            print()
-            print("GAME OVER")
-            break
+    if lives == 0:
+        print()
+        print("GAME OVER")
+        break
+# Funtion to print an updated version hidden_word
+def print_hidden_word():
+    print(" ".join([letter for letter in hidden_word])) # .join converts hidden_word in string
 
+# Function for user to attempt to guess the word
+def input_word():
+    answer = input("Can you guess the word? (yes/no): ")
+    if answer is yes:
+        attempt = input("Type the word: ")
+    else:
+        input()
+
+
+
+
+
+# Function to check whether the user's guessed the word
+def check_user_attempt():
+    for index, letters in enumerate(attempt):
+        if user_attempt is selected_word:
+            user_attempt[index] == selected_word[index]
